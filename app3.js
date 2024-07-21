e6b76c42c7880df628b8ddbcd80018e9db291d1c
TatianaKarpovaP <119897441+TatianaKarpovaP@users.noreply.github.com>
2024-07-21 16:39:07 +0200

    Create app3.js

---
+++ app3.js
@@ -0,0 +1,49 @@
+console.log('Is this working?');
+
+let viz;
+
+const url = "https://public.tableau.com/views/Lloyds_17120130643130/Dashboard1";
+
+
+const vizContainer = document.getElementById('vizContainer');
+const options = {
+    hideTabs: true,
+    height: 900,
+    width: 1100,
+    onFirstInteraction: function() {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+//create a function to generate the viz element
+function initViz() {
+    console.log('Executing the initViz function!');
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+// run the initViz function when the page loads
+document.addEventListener("DOMContentLoaded", initViz);
+
+const exportPDF = document.getElementById('exportPDF');
+const exportImage = document.getElementById('exportImage');
+
+
+//click on the pdf button to generate pdf of dashboard
+function generatePDF() {
+    viz.showExportPDFDialog()
+}
+
+exportPDF.addEventListener("click", function () {
+    generatePDF();
+  });
+
+//click on image to generate image of dashboard
+function generateImage() {
+    viz.showExportImageDialog()
+}
+
+exportImage.addEventListener("click", function () {
+    generateImage();
+  });
